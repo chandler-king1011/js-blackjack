dabb268fa4e8f09fbb6a23c976c6130202e57fe4
Chandler King <ckmotox356@gmail.com>
2020-04-18 16:54:35 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let userScore = document.querySelector(".user-score");
+let botScore = document.querySelector(".bot-score");
+
+
+const handleHit = () => {
+
+}
+
+const handleStand = () => {
+
+}
+
+const handleDeal = () => {
+
+}
